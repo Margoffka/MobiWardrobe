989800b5ba5c13975fcf946ade5e7440be5191dd
Margoffka <margousha8@gmail.com>
2022-12-07 23:29:00 +0300

    fix photo add function

--- app/src/main/java/com/mobiwardrobe/mobiwardrobe/UploadImageActivity.java
+++ app/src/main/java/com/mobiwardrobe/mobiwardrobe/UploadImageActivity.java
@@ -1,16 +1,10 @@
 package com.mobiwardrobe.mobiwardrobe;
 
 import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Parcelable;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
@@ -22,7 +16,6 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -34,20 +27,14 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 public class UploadImageActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE_REQUEST = 1;
     private static final int REQUEST_CODE_PERMISSION_RECEIVE_CAMERA = 102;
     private static final int REQUEST_CODE_TAKE_PHOTO = 103;
+    private static final int CAMERA_REQUEST_CODE = 100;
 
     private Button mButtonChooseImage;
     private Button mButtonUpload;
@@ -58,8 +45,8 @@ public class UploadImageActivity extends AppCompatActivity {
     private EditText mTest;
     private Button mButtonChoosePhoto;
 
-    private File mTempPhoto;
-    private String newImageUri = "";
+//    private File mTempPhoto;
+//    private String newImageUri = "";
 
     private Uri mImageUri;
 
@@ -91,10 +78,11 @@ public class UploadImageActivity extends AppCompatActivity {
         mButtonChoosePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addPhoto();
+//                Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(open_camera, CAMERA_REQUEST_CODE);
+                CropImage.activity().start(UploadImageActivity.this);
             }
         });
-
 
         mButtonChooseImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -132,6 +120,11 @@ public class UploadImageActivity extends AppCompatActivity {
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
+//    public void onChoosePhoto(View v) {
+//
+//    }
+
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -145,21 +138,21 @@ public class UploadImageActivity extends AppCompatActivity {
 //            Log.d("TAG", mImageUri.toString());
             Picasso.with(this).load(mImageUri).into(mImageView);
         }
-    }
 
-   /* private void openImageChooser() {
-        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                            mImageUri = result.getData().getData();
-                            Picasso.with(this).load(mImageUri).into(mImageView);
-                            mImageView.setImageURI(mImageUri);
-                        }
-                    }
-                });
-    }*/
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if (resultCode == RESULT_OK){
+                mImageUri = result.getUri();
+                mImageView.setImageURI(mImageUri);
+            }
+
+            else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Toast.makeText(this, "Possible error: " + error, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     private String getFileExtension(Uri uri) {
         ContentResolver cR = getContentResolver();
@@ -195,6 +188,7 @@ public class UploadImageActivity extends AppCompatActivity {
                                     mDatabaseRef.child(uploadId).setValue(upload);
                                 }
                             });
+                            finish();
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
@@ -213,7 +207,6 @@ public class UploadImageActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
         }
-        //finish();
     }
 
 
@@ -221,95 +214,4 @@ public class UploadImageActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
-    //Метод для добавления фото
-    private void addPhoto() {
-
-        //Проверяем разрешение на работу с камерой
-        boolean isCameraPermissionGranted = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
-        //Проверяем разрешение на работу с внешнем хранилещем телефона
-        boolean isWritePermissionGranted = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-
-        //Если разрешения != true
-        if(!isCameraPermissionGranted || !isWritePermissionGranted) {
-
-            String[] permissions;//Разрешения которые хотим запросить у пользователя
-
-            if (!isCameraPermissionGranted && !isWritePermissionGranted) {
-                permissions = new String[] {android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
-            } else if (!isCameraPermissionGranted) {
-                permissions = new String[] {android.Manifest.permission.CAMERA};
-            } else {
-                permissions = new String[] {android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
-            }
-            //Запрашиваем разрешения у пользователя
-            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_PERMISSION_RECEIVE_CAMERA);
-        } else {
-            //Если все разрешения получены
-            try {
-                mTempPhoto = createTempImageFile(getExternalCacheDir());
-                newImageUri = mTempPhoto.getAbsolutePath();
-
-                //Создаём лист с интентами для работы с изображениями
-                List<Intent> intentList = new ArrayList<>();
-                Intent chooserIntent = null;
-
-
-                Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                takePhotoIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mTempPhoto));
-
-                intentList = addIntentsToList(this, intentList, pickIntent);
-                intentList = addIntentsToList(this, intentList, takePhotoIntent);
-
-                if (!intentList.isEmpty()) {
-                    chooserIntent = Intent.createChooser(intentList.remove(intentList.size() - 1),"Choose your image source");
-                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentList.toArray(new Parcelable[]{}));
-                }
-
-                /*После того как пользователь закончит работу с приложеним(которое работает с изображениями)
-                 будет вызван метод onActivityResult
-                */
-                startActivityForResult(chooserIntent, REQUEST_CODE_TAKE_PHOTO);
-            } catch (IOException e) {
-                Log.e("ERROR", e.getMessage(), e);
-            }
-        }
-    }
-
-    /*
-    File storageDir -  абсолютный путь к каталогу конкретного приложения на
-    основном общем /внешнем устройстве хранения, где приложение может размещать
-    файлы кеша, которыми он владеет.
-   */
-    public static File createTempImageFile(File storageDir) throws IOException {
-
-        // Генерируем имя файла
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());//получаем время
-        String imageFileName = "photo_" + timeStamp;//составляем имя файла
-
-        //Создаём файл
-        return File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-    }
-
-    /*
-    Метод для добавления интента в лист интентов
-    */
-    public static List<Intent> addIntentsToList(Context context, List<Intent> list, Intent intent) {
-        List<ResolveInfo> resInfo = context.getPackageManager().queryIntentActivities(intent, 0);
-        for (ResolveInfo resolveInfo : resInfo) {
-            String packageName = resolveInfo.activityInfo.packageName;
-            Intent targetedIntent = new Intent(intent);
-            targetedIntent.setPackage(packageName);
-            list.add(targetedIntent);
-        }
-        return list;
-    }
-
 }
